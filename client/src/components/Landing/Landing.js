67a2ebf2e6ab58332c12b4d2be70a24108aaf10c
Disquietude <danny93ngo@gmail.com>
2020-03-29 00:57:39 -0400

    Set up initial folder structure

---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Landing = () => (
+  <div>
+    <Typography variant="h1" align="center">Landing Page</Typography>
+  </div>
+);
+
+export default Landing;
---
+++ client/src/styles/Theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "'Open Sans', sans-serif"
+  }
+});
+
+export default theme;
