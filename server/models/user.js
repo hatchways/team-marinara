46f8ba8c325370dc6fbf118bef537732255eef4b
Darren Greenfield <darren.greenfield@gmail.com>
2020-03-30 15:00:15 -0700

    Update User model to hold gmail token

--- server/models/user.js
+++ server/models/user.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  gmailToken: { type: String },
 });
 
 module.exports = mongoose.model("User", UserSchema);
--- server/routes/gmail-auth.js
+++ server/routes/gmail-auth.js
@@ -5,7 +5,6 @@ const { google } = require("googleapis");
 // If modifying these scopes, delete token.json.
 const SCOPES = [
   "https://www.googleapis.com/auth/gmail.readonly",
-  "https://www.googleapis.com/auth/gmail.modify",
   "https://www.googleapis.com/auth/gmail.compose",
   "https://www.googleapis.com/auth/gmail.send",
 ];
@@ -13,36 +12,6 @@ const SCOPES = [
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = "token.json";
-
-// Load client secrets from a local file.
-fs.readFile("../gmail-secret.json", (err, content) => {
-  if (err) {
-    return console.log("Error loading client secret file:", err);
-  }
-
-  // Authorize the client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), getAuth);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.web;
-  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) {
-      return getNewToken(oAuth2Client, callback);
-    }
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -75,5 +44,35 @@ function getNewToken(oAuth2Client, callback) {
     });
   });
 }
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) {
+      return getNewToken(oAuth2Client, callback);
+    }
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+// Load client secrets from a local file.
+fs.readFile("./gmail-secret.json", (err, content) => {
+  if (err) {
+    return console.log("Error loading client secret file:", err);
+  }
+
+  // Authorize the client with credentials, then call the Gmail API.
+  authorize(JSON.parse(content), getAuth);
+});
 
 function getAuth(auth) {}
