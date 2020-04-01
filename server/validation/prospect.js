a971b01393f38bf2768865e8810c87d52235422d
Airen <nyzrus@gmail.com>
2020-04-01 15:03:21 -0400

    Validation updates

--- server/validation/prospect.js
+++ server/validation/prospect.js
@@ -5,28 +5,25 @@ const { STATUS } = require("../models/Prospect")
 module.exports = function validateProspectInput(data) {
   const errors = {};
 
-// Convert empty fields to an empty string so we can use validator functions
-  data.firstName = (!data.firstName) ? "" : data.firstName;
-  data.lastName = (!data.lastName) ? "" : data.lastName;
-  data.email = (!data.email) ?  "" : data.email;
-  data.status = (!data.status) ? "" : data.status;
 // Name checks
-  if (Validator.isEmpty(data.firstName)) {
+  if ((!data.firstName)) {
     errors.firstName = "First name is required";
   }
-  if (Validator.isEmpty(data.lastName)) {
+  if (!data.lastName) {
     errors.lastName = "Last name is required";
   }
   
 // Email checks
-  if (Validator.isEmpty(data.email)) {
+  if (!data.email) {
     errors.email = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
 
 // Status checks  
-if (!Object.values(STATUS).includes(data.status)) {
+if (!data.status) {
+    errors.status = "Status is required";
+} else if (!Object.values(STATUS).includes(data.status)) {
     errors.status = "Invalid Prospect status";
 }
 
