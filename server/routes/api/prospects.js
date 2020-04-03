8d2df9c7790e1ad97f5b1886bd924ce808f9a5f4
Darren Greenfield <darren.greenfield@gmail.com>
2020-04-03 14:53:55 -0700

    Add try catch to async await code

--- server/routes/api/prospects.js
+++ server/routes/api/prospects.js
@@ -63,7 +63,7 @@ router.get("/:id", (req, res) => {
 // @route PUT /api/prospects/{id}
 // @desc Update a Prospect object
 // @access Authenticated Users
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   let id = req.params.id;
 
   const { errors, isValid } = validateProspectInput(req.body);
@@ -72,37 +72,42 @@ router.put("/:id", (req, res) => {
     return res.status(400).json(errors);
   }
 
-  Prospect.findById(id, async function(err, prospect) {
-    if (!prospect) {
-      res.status(404).send({ id: `Prospect with id ${id} is not found` });
-    } else {
-      //if updating email field, check if Prospect with new email alredy exists
-      if (req.body.email !== prospect.email) {
-        const emailExists = await Prospect.findOne({
-          email: req.body.email
-        });
-        if (emailExists) {
-          return res.status(400).json({
-            email: "Cannot update email address. Email address already exists"
+  try {
+    await Prospect.findById(id, async function(err, prospect) {
+      if (!prospect) {
+        res.status(404).send({ id: `Prospect with id ${id} is not found` });
+      } else {
+        //if updating email field, check if Prospect with new email alredy exists
+        if (req.body.email !== prospect.email) {
+          const emailExists = await Prospect.findOne({
+            email: req.body.email
           });
+          if (emailExists) {
+            return res.status(400).json({
+              email: "Cannot update email address. Email address already exists"
+            });
+          }
         }
+
+        prospect.firstName = req.body.firstName;
+        prospect.lastName = req.body.lastName;
+        prospect.lastContacted = req.body.lastContacted;
+        prospect.ownedBy = req.body.ownedBy;
+        prospect.status = req.body.status;
+        prospect.email = req.body.email;
+
+        prospect
+          .save()
+          .then(prospect => {
+            res.json(prospect);
+          })
+          .catch(err => console.log(err));
       }
-
-      prospect.firstName = req.body.firstName;
-      prospect.lastName = req.body.lastName;
-      prospect.lastContacted = req.body.lastContacted;
-      prospect.ownedBy = req.body.ownedBy;
-      prospect.status = req.body.status;
-      prospect.email = req.body.email;
-
-      prospect
-        .save()
-        .then(prospect => {
-          res.json(prospect);
-        })
-        .catch(err => console.log(err));
-    }
-  });
+    });
+  } catch (error) {
+    console.log("Error editing prospect: ", error);
+    res.status(500).send({ error: error });
+  }
 });
 
 // @route DELETE /api/prospects/{id}
