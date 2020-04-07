77bb64e69db4604b6ddbd62488cb846132ceec4c
Darren Greenfield <darren.greenfield@gmail.com>
2020-04-07 10:56:41 -0700

    Restructure Campaign Schema based on Shums feedback

--- server/models/campaign.js
+++ server/models/campaign.js
@@ -5,6 +5,14 @@ const CampaignSchema = new Schema({
   name: { type: String, required: true },
   created: { type: Date, default: Date.now, required: true },
   prospects: [{ type: Schema.Types.ObjectId, ref: "Prospect", index: true }],
+  stepsSummary: {
+    sent: { type: Number, default: 0, required: true },
+    delivered: { type: Number, default: 0, required: true },
+    opened: { type: Number, default: 0, required: true },
+    clicked: { type: Number, default: 0, required: true },
+    replied: { type: Number, default: 0, required: true },
+    bounced: { type: Number, default: 0, required: true }
+  },
   steps: [
     {
       name: { type: String, required: true },
@@ -17,11 +25,31 @@ const CampaignSchema = new Schema({
       subject: { type: String },
       content: { type: String },
       created: { type: Date, default: Date.now, required: true },
-      stepDetail: {
-        type: Schema.Types.ObjectId,
-        ref: "StepDetail",
-        index: true
-      }
+      prospects: [
+        {
+          prospect: {
+            type: Schema.Types.ObjectId,
+            ref: "Prospect",
+            required: true,
+            index: true
+          },
+          status: {
+            type: String,
+            required: true,
+            enum: [
+              "Draft",
+              "Sent",
+              "Delivered",
+              "Opened",
+              "Clicked",
+              "Replied",
+              "Bounced",
+              "Opted out"
+            ],
+            default: "Draft"
+          }
+        }
+      ]
     }
   ]
 });
