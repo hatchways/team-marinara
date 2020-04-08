0d2e01f954f2964fb96ab26609da2b7111e5bfa3
Darren Greenfield <darren.greenfield@gmail.com>
2020-04-08 13:51:04 -0700

    Add put and get campaign routes

---
+++ server/routes/api/campaigns.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const passport = require("passport");
+
+const Campaign = require("../../models/campaign");
+
+// @route POST /api/campaigns
+// @desc Create a Campaign object. Requires 'name' (campaign name) and 'ownedBy' (a userId)
+// @access Authenticated Users
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      //*********** TO DO *****************
+      //Campaign field validation
+      //const { errors, isValid } = validateCampaignInput(req.body);
+
+      // if (!isValid) {
+      //   return res.status(400).json(errors);
+      // }
+
+      const { name, ownedBy } = req.body;
+      const newCampaign = new Campaign({
+        name,
+        ownedBy
+      });
+      const campaign = await newCampaign.save();
+      res.json(campaign);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// @route GET /api/campaigns
+// @desc Get all campaigns of logged in user. Requires no params
+// @access Authenticated Users
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const userId = req.user.id;
+
+      const campaigns = await Campaign.find({ ownedBy: userId });
+      res.json(campaigns);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// @route GET /api/campaigns
+// @desc Get specified campaign of logged in user. Requires 'campaignId'
+// @access Authenticated Users
+router.get(
+  "/campaign",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const { campaignId } = req.body;
+
+      const campaign = await Campaign.find({
+        _id: campaignId,
+        ownedBy: userId
+      });
+      res.json(campaign);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+module.exports = router;
