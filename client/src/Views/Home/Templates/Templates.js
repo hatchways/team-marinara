0e57e1e465f39e1b5094180a5151a71ba630bbc9
Airen <nyzrus@gmail.com>
2020-04-15 17:10:45 -0400

    Initial Templates FE

--- client/src/Views/Home/Templates/Templates.js
+++ client/src/Views/Home/Templates/Templates.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { Grid, Typography, withStyles } from "@material-ui/core";
 
+import ProspectSidebar from "../Prospects/ProspectSidebar";
+import TemplatesHeader from "./TemplatesHeader";
+import TemplatesTable from "./TemplatesTable";
+
 const styles = () => ({
   root: {
     flexGrow: 1
   }
 });
 
-const Templates = props => (
-  <Grid item container className={props.classes.root} justify="center">
-    <Typography variant="h1">Templates Page</Typography>
-  </Grid>
-);
+const Templates = props => {
+  
+  return (
+    <Grid item container className={props.classes.root}>
+      <Grid item xs={3}>
+            <ProspectSidebar />
+        </Grid>
+        
+        <Grid item xs={9}>
+          <TemplatesHeader />
+          <TemplatesTable />
+        </Grid>
+    </Grid>
+  );
+  }
 
 export default withStyles(styles)(Templates);
