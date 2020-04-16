a47ef5e667d3697655e6698819de0e7c36d4ada6
Disquietude <danny93ngo@gmail.com>
2020-04-16 17:37:09 -0400

    Added DataColumn component

--- client/src/Views/Home/Campaigns/Campaign/Step.js
+++ client/src/Views/Home/Campaigns/Campaign/Step.js
@@ -3,6 +3,8 @@ import { Grid, Typography, makeStyles } from "@material-ui/core";
 
 import colors from "Components/Styles/Colors";
 
+import DataColumn from "Components/DataColumn/DataColumn";
+
 const useStyles = makeStyles({
   root: {
     backgroundColor: colors.white,
@@ -11,6 +13,13 @@ const useStyles = makeStyles({
     border: `2px solid ${colors.midGray}`,
     width: "100%",
     marginBottom: "2rem"
+  },
+  nameContainer: {
+    flexGrow: 1
+  },
+  name: {
+    fontSize: "1rem",
+    fontWeight: "bold"
   }
 });
 
@@ -18,7 +27,10 @@ const Step = props => {
   const classes = useStyles();
   return (
     <Grid item container alignItems="center" className={classes.root}>
-      <Typography>{props.step.name}</Typography>
+      <Grid item className={classes.nameContainer}>
+        <Typography className={classes.name}>{props.step.name}</Typography>
+      </Grid>
+      <DataColumn label="Prospects" value={props.step.prospects.length} />
     </Grid>
   );
 };
