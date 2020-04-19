e214c2439fdc915a8d3ef873c7ca1e1a78f6cca1
Darren Greenfield <darren.greenfield@gmail.com>
2020-04-19 11:41:57 -0700

    Initial gcloud set up

--- server/app.js
+++ server/app.js
@@ -12,12 +12,12 @@ const passport = require("passport");
 require("./config/passport")(passport);
 
 // Connect to the database
-const mongoDB = `${config.mongoURI}:${config.mongoPort}/${config.mongoDB}`;
+const mongoDB = `${config.mongoURI}`;
 mongoose
   .connect(mongoDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    useCreateIndex: true
   })
   .then(() => {
     console.log("Connected to database...");
@@ -37,14 +37,15 @@ app.use(express.static(join(__dirname, "public")));
 app.use(passport.initialize());
 
 app.use("/", routes);
+// app.use(/^(?!.*_ah).*$/, routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,14 @@
 const config = {
-  mongoURI: process.env.MONGO_URI || "mongodb://localhost",
-  mongoPort: process.env.MONGO_PORT || "27017",
-  mongoDB: process.env.MONGO_DB || "mail-sender-dev",
+  mongoURI:
+    process.env.MONGO_URI ||
+    "mongodb+srv://umbauk:lasgdar82@cluster0-trix2.gcp.mongodb.net/mail-sender-dev?retryWrites=true&w=majority", //"mongodb://localhost:27017/mail-sender-dev",
   appSecret: process.env.APP_SECRET || "secret",
   googleClientSecret:
     process.env.GOOGLE_CLIENT_SECRET ||
     require("./gmail-secret.json").client_secret,
   googleClientId:
-    process.env.GOOGLE_CLIENT_ID || require("./gmail-secret.json").client_id
+    process.env.GOOGLE_CLIENT_ID || require("./gmail-secret.json").client_id,
+  tmpDir: process.env.TMP_DIR || "../server/tmp/uploads"
 };
 
 module.exports = config;
