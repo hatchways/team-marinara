28aacd5411b4c68f3e16f850dbeedda213c2986a
Darren Greenfield <42155812+umbauk@users.noreply.github.com>
2020-04-20 14:46:17 -0700

    Prepare app for GCP deployment (#78)
    * Initial gcloud set up
    
    * Add app.yaml deployment config file and update README with deployment instructions
    
    * Add more details to README
    
    * Add more details to README
    
    * Fix mongoURI
    
    * Delete commented out code

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
 const config = {
-  mongoURI: process.env.MONGO_URI || "mongodb://localhost",
-  mongoPort: process.env.MONGO_PORT || "27017",
-  mongoDB: process.env.MONGO_DB || "mail-sender-dev",
+  mongoURI:
+    process.env.MONGO_URI || "mongodb://localhost:27017/mail-sender-dev",
   appSecret: process.env.APP_SECRET || "secret",
   googleClientSecret:
     process.env.GOOGLE_CLIENT_SECRET ||
     require("./gmail-secret.json").client_secret,
   googleClientId:
-    process.env.GOOGLE_CLIENT_ID || require("./gmail-secret.json").client_id
+    process.env.GOOGLE_CLIENT_ID || require("./gmail-secret.json").client_id,
+  tmpDir: process.env.TMP_DIR || "../server/tmp/uploads"
 };
 
 module.exports = config;
--- server/routes/api/prospects.js
+++ server/routes/api/prospects.js
@@ -1,115 +1,143 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const multer = require('multer');
-const csv = require('csv-parser');
-const fs = require('fs');
-
+const multer = require("multer");
+const csv = require("csv-parser");
+const fs = require("fs");
 const passport = require("passport");
 
-const {validateProspectInput, validateFile} = require("../../validation/prospect");
+const {
+  validateProspectInput,
+  validateFile
+} = require("../../validation/prospect");
 const Prospect = require("../../models/Prospect");
-const { processCsvData } = require("../../utils/csv/csvProcess")
+const { processCsvData } = require("../../utils/csv/csvProcess");
+const tmpDir = require("../../config/config").tmpDir;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../server/tmp/uploads')
+    cb(null, tmpDir);
   },
   filename: function (req, file, cb) {
-    cb(null, 'csv-upload' + '-' + Date.now())
+    cb(null, "csv-upload" + "-" + Date.now());
   }
 });
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 // @route POST /api/prospects
 // @desc Create a Prospect object
 // @access Authenticated Users
-router.post("/", passport.authenticate("jwt", { session: false }),
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     //Prospect field validation
     const ownedBy = req.user.id;
     const { errors, isValid } = validateProspectInput(req.body);
 
-      if (!isValid) {
-          return res.status(400).json(errors);
-      }
-      Prospect.findOne({ email: req.body.email }).then(prospect => {
-          if (prospect) {
-            return res.status(400).json({ email: "Prospect with this email address already exists" });
-          } else {
-            if(ownedBy !== null && !mongoose.Types.ObjectId.isValid(ownedBy)){
-              return res.status(400).json({ ownedBy: "Invalid user id provided for ownedBy"});
-            }
-            const {firstName, lastName, email, status} = req.body;
-            const newProspect = new Prospect({firstName, lastName, email, ownedBy, status});
-      newProspect
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    Prospect.findOne({ email: req.body.email }).then(prospect => {
+      if (prospect) {
+        return res
+          .status(400)
+          .json({ email: "Prospect with this email address already exists" });
+      } else {
+        if (ownedBy !== null && !mongoose.Types.ObjectId.isValid(ownedBy)) {
+          return res
+            .status(400)
+            .json({ ownedBy: "Invalid user id provided for ownedBy" });
+        }
+        const { firstName, lastName, email, status } = req.body;
+        const newProspect = new Prospect({
+          firstName,
+          lastName,
+          email,
+          ownedBy,
+          status
+        });
+        newProspect
           .save()
           .then(prospect => res.json(prospect))
           .catch(err => console.log(err));
       }
     });
-});
+  }
+);
 
 // @route GET /api/prospects/{id}
 // @desc Get a Prospect object
 // @access Authenticated Users
-router.get("/:id", passport.authenticate("jwt", { session: false }),
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const ownedBy = req.user.id;
     const id = req.params.id;
-    Prospect.findById(id, function(err, prospect) {
+    Prospect.findById(id, function (err, prospect) {
       if (!prospect) {
         res.status(404).send({ id: `Prospect with id ${id} is not found` });
       } else if (ownedBy != prospect.ownedBy) {
-        res.status(403).send({ ownedBy : `Prospect with id ${id} is not ownedBy user ${ownedBy}`});
+        res.status(403).send({
+          ownedBy: `Prospect with id ${id} is not ownedBy user ${ownedBy}`
+        });
         return;
       } else {
         res.json(prospect);
       }
     });
-});
+  }
+);
 
 // @route GET /api/prospects
 // @desc Get a List of Prospect objects for a User
 // @access Authenticated Users
-router.get("/", passport.authenticate("jwt", { session: false }),
-  async function (req, res)  {
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async function (req, res) {
     const ownedBy = req.user.id;
     let results;
 
-    if(mongoose.Types.ObjectId.isValid(ownedBy)) {
+    if (mongoose.Types.ObjectId.isValid(ownedBy)) {
       ownedById = mongoose.Types.ObjectId(ownedBy);
-      try{
-        results = await Prospect.find({ownedBy: ownedBy});
-      } catch(err) {
+      try {
+        results = await Prospect.find({ ownedBy: ownedBy });
+      } catch (err) {
         console.log(err);
       }
     }
     res.status(200).send(results);
-});
+  }
+);
 
 // @route PUT /api/prospects/{id}
 // @desc Update a Prospect object
 // @access Authenticated Users
-router.put("/:id", passport.authenticate("jwt", { session: false }),
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     const id = req.params.id;
     const ownedBy = req.user.id;
 
-    const { errors, isValid } = validateProspectInput(req.body); 
-    const {firstName, lastName, email, status, lastContacted} = req.body;
+    const { errors, isValid } = validateProspectInput(req.body);
+    const { firstName, lastName, email, status, lastContacted } = req.body;
 
     if (!isValid) {
       return res.status(400).json(errors);
     }
 
     try {
-      await Prospect.findById(id, async function(err, prospect) {
+      await Prospect.findById(id, async function (err, prospect) {
         if (!prospect) {
           res.status(404).send({ id: `Prospect with id ${id} is not found` });
         } else {
-          if(prospect.ownedBy != ownedBy) {
-            res.status(403).send({ ownedBy : `Prospect with id ${id} is not ownedBy user ${ownedBy}`});
+          if (prospect.ownedBy != ownedBy) {
+            res.status(403).send({
+              ownedBy: `Prospect with id ${id} is not ownedBy user ${ownedBy}`
+            });
             return;
           }
           //if updating email field, check if Prospect with new email alredy exists
@@ -119,59 +147,63 @@ router.put("/:id", passport.authenticate("jwt", { session: false }),
             });
             if (emailExists) {
               return res.status(400).json({
-                email: "Cannot update email address. Email address already exists"
+                email:
+                  "Cannot update email address. Email address already exists"
               });
             }
           }
-          
+
           prospect.firstName = firstName;
           prospect.lastName = lastName;
           prospect.lastContacted = lastContacted;
           prospect.status = status;
           prospect.email = email;
 
-            prospect
-              .save()
-              .then(prospect => {
-                res.json(prospect);
-              })
-              .catch(err => console.log(err));
+          prospect
+            .save()
+            .then(prospect => {
+              res.json(prospect);
+            })
+            .catch(err => console.log(err));
         }
       });
     } catch (error) {
       console.log("Error editing prospect: ", error);
       res.status(500).send({ error: error });
     }
-});
+  }
+);
 
 // @route DELETE /api/prospects/{id}
 // @desc Delete a Prospect object
 // @access Authenticated Users
-router.delete("/:id", passport.authenticate("jwt", { session: false }),
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
-    
     const ownedBy = req.user.id;
     let id = req.params.id;
 
-    if(mongoose.Types.ObjectId.isValid(id)) {
+    if (mongoose.Types.ObjectId.isValid(id)) {
       id = mongoose.Types.ObjectId(id);
     } else {
       res.status(404).send({ id: `Prospect with id ${id} not found.` });
       return;
     }
 
-    try{
+    try {
       const prospect = await Prospect.findById(id);
-      if(prospect && prospect.ownedBy != ownedBy) {
-        res.status(403).send({ ownedBy : `Prospect with id ${id} is not ownedBy user ${ownedBy}`});
+      if (prospect && prospect.ownedBy != ownedBy) {
+        res.status(403).send({
+          ownedBy: `Prospect with id ${id} is not ownedBy user ${ownedBy}`
+        });
         return;
       }
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-    
 
-    Prospect.findOneAndDelete({ _id: id }, function( err,removeResult) {
+    Prospect.findOneAndDelete({ _id: id }, function (err, removeResult) {
       if (err) {
         res.status(400).send(err);
       }
@@ -182,46 +214,51 @@ router.delete("/:id", passport.authenticate("jwt", { session: false }),
         res.send({ id: id });
       }
     });
-});
+  }
+);
 
 // @route POST /api/prospects/upload
 // @desc Upload Prospects with a csv file
 // @access Authenticated Users
-router.post("/upload", passport.authenticate("jwt", { session: false }),
-  upload.single('file'), async (req, res) => {
+router.post(
+  "/upload",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("file"),
+  async (req, res) => {
     console.log(req);
     const csvData = [];
     const file = req.file;
     const userId = req.user.id;
 
     const { errors, isValid } = validateFile(file);
-    if(!isValid) {
-        return res.status(400).send(errors);
+    if (!isValid) {
+      return res.status(400).send(errors);
     }
 
     fs.createReadStream(file.path)
-        .pipe(csv())
-        .on('data', (row) => {
-          csvData.push(row);
-        })
-        .on("end",  async () => {
-          fs.unlinkSync(file.path);
-          try{
-            const count = await processCsvData(csvData, userId);
-            res.status(200).send(count);
-          } catch(err){
-            const error = {
-              success : false,
-              message : "CSV failed to upload",
-              details : err.message
-            }
-            res.status(400).send(error);
-          }
-        })
-        .on('error', function(err) {
-            console.log(err);
-            res.status(400).send(err);
-        });
-});
+      .pipe(csv())
+      .on("data", row => {
+        csvData.push(row);
+      })
+      .on("end", async () => {
+        fs.unlinkSync(file.path);
+        try {
+          const count = await processCsvData(csvData, userId);
+          res.status(200).send(count);
+        } catch (err) {
+          const error = {
+            success: false,
+            message: "CSV failed to upload",
+            details: err.message
+          };
+          res.status(400).send(error);
+        }
+      })
+      .on("error", function (err) {
+        console.log(err);
+        res.status(400).send(err);
+      });
+  }
+);
 
 module.exports = router;
