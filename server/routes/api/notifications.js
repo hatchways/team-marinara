88be43766489f8315083cd47e998d0f34ed55282
Darren Greenfield <darren.greenfield@gmail.com>
2020-04-22 15:45:28 -0700

    Tidy up code

--- server/routes/api/notifications.js
+++ server/routes/api/notifications.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { google } = require("googleapis");
-const passport = require("passport");
 const { pubSubSubscriptionName } = require("../../config/config");
 
 const {
@@ -12,7 +11,6 @@ const {
 const User = require("../../models/user");
 const Campaign = require("../../models/campaign");
 const Step = require("../../models/step");
-const Prospect = require("../../models/Prospect");
 const Thread = require("../../models/thread");
 
 // @route POST /api/notifications
@@ -29,32 +27,65 @@ router.post("/", async (req, res) => {
     const decodedData = await JSON.parse(
       Buffer.from(req.body.message.data, "base64").toString()
     );
-    console.log("decodedData:", decodedData);
     const userEmail = decodedData.emailAddress;
     const historyId = decodedData.historyId;
 
-    console.log("userEmail:", userEmail);
     const user = await User.findOne(
       { email: userEmail },
       "gmailToken gmailHistoryId"
     );
     console.log("user:", user);
 
-    // Get emails with history.list()
     await getEmail(user.gmailToken, user.gmailHistoryId);
 
     // Replace last synced historyId for user with latest
     user.gmailHistoryId = historyId;
     await user.save();
 
-    // Gmail requires a 200 status acknowledgment
-    console.log("FINISHED, sending res(200)...");
+    // Gmail requires a 200 status acknowledgment to stop sending notifications
+    console.log(
+      `FINISHED processing notificatio for ${userEmail}, sending res(200)...`
+    );
     res.status(200).send("OK");
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Error saving campaign" });
   }
 });
+
+const getNewThreadIds = async (gmail, startHistoryId) => {
+  // get list of email ids that have been received since last check
+  const history = await gmail.users.history.list({
+    userId: "me",
+    startHistoryId: startHistoryId,
+    historyTypes: "messageAdded"
+  });
+
+  if (!history.data.history) {
+    console.log("No new messages");
+    return false;
+  }
+  const messageData = history.data.history.flat();
+
+  // get only the threads that have received new messages
+  const messagesAdded = messageData
+    .filter(thread => thread.messagesAdded)
+    .map(thread => thread.messagesAdded);
+
+  if (messagesAdded.length === 0) {
+    console.log("No new messages added");
+    return false;
+  }
+
+  // get just the threadIds of the messages
+  return messagesAdded
+    .map(message => {
+      return message.map(msg => {
+        return msg.message.threadId;
+      });
+    })
+    .flat();
+};
 
 /*
  * Gets email
@@ -71,101 +102,20 @@ const getEmail = async (gmailToken, startHistoryId) => {
   oAuth2Client.setCredentials({ refresh_token: gmailToken });
   const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
 
-  // get list of emailIds that have been received
-  const history = await gmail.users.history.list({
-    userId: "me",
-    startHistoryId: startHistoryId
-    // labelId: gmailLabelId
-  });
+  const threadIds = await getNewThreadIds(gmail, startHistoryId);
+  if (threadIds === false) return false;
 
-  // console.log("history:", history);
-  // console.log("history.data:", history.data);
-  if (!history.data.history) {
-    // no new messages
-    console.log("No new messages");
-    return true;
-  }
-
-  // See https://developers.google.com/gmail/api/v1/reference/users/history/list for rough data structure of history
-  const messageData = history.data.history.flat();
-  // console.log(">>>>>>messageData:", messageData);
-
-  // get only the threads that have received new messages
-  const messagesAdded = messageData
-    .filter(thread => thread.messagesAdded)
-    .map(thread => thread.messagesAdded);
-
-  if (messagesAdded.length === 0) {
-    // no new messages
-    console.log("No new messages added");
-    return true;
-  }
-  // console.log(">>>>>>>>messagesAdded:", messagesAdded);
-
-  const msgThreadIds = messagesAdded
-    .map(message => {
-      // console.log("message:", message);
-      return message.map(msg => {
-        // console.log("msg:", msg);
-        return { messageId: msg.message.id, threadId: msg.message.threadId };
-      });
-    })
-    .flat();
-  // console.log(">>>>>>>messageIds:", msgThreadIds);
-
-  // Find the prospects with this Thread ID
+  // Find the thread records that match the ids
   const threadRecords = await Promise.all(
-    msgThreadIds.map(async msgThreadId => {
+    threadIds.map(async threadId => {
       const threadRecord = await Thread.findOne({
-        threadId: msgThreadId.threadId
+        threadId: threadId
       });
       return threadRecord;
     })
   );
 
-  // // get latest threads to see which campaigns and prospects they are from
-  // const threads = await Promise.all(
-  //   msgThreadIds.map(async messageThreadId => {
-  //     const thread = await gmail.users.threads.get({
-  //       userId: "me",
-  //       id: messageThreadId.threadId,
-  //       format: "metadata" // return only headers, ids and labels
-  //     });
-  //     thread.messageId = messageThreadId.messageId;
-  //     return thread;
-  //   })
-  // );
-  // threads.forEach(thread => {
-  //   console.log("thread.data:", thread.messages);
-  // });
-
-  // // \u003cdarrengreenfield555@gmail.com\u003e
-  // // get labelId which identifies the campaign and prospect email address
-  // const emailsData = threads.map(thread => {
-  //   const labelId = thread.messages[0].labelIds.find(labelId =>
-  //     labelId.startsWith("mscId")
-  //   );
-
-  //   const newMessage = thread.messages.find(
-  //     message => message.id === thread.messageId
-  //   );
-
-  //   const fromObj = newMessage.payload.headers.find(
-  //     header => header["name"] === "From"
-  //   );
-  //   console.log("fromObj.value", fromObj["value"]);
-  //   const emailAddr = fromObj["value"].match(/<(.*)>/);
-  //   console.log("email address:", emailAddr[1]);
-  //   return {
-  //     labelId: labelId,
-  //     prospectEmailAddr: emailAddr[1]
-  //   };
-  // });
-  // for (const email of emailsData) {
-  //   console.log(">>>>>>>>>>>>>emailsData:", email);
-  // }
-
-  // Update the prospect status in the step
+  // Update the status and replied counters of relevant records
   await Promise.all(
     threadRecords.map(async threadRecord => {
       if (threadRecord) {
@@ -179,12 +129,12 @@ const getEmail = async (gmailToken, startHistoryId) => {
         campaign.prospects[prospectIndex].status = "Replied";
         campaign.stepsSummary.replied++;
 
-        // update the Step status
         step.summary.replied++;
         const stepProspectIndex = step.prospects.findIndex(prospect =>
           prospect.prospectId.equals(threadRecord.prospectId)
         );
         step.prospects[stepProspectIndex].status = "Replied";
+
         console.log("SAVING.........");
         await campaign.save();
         await step.save();
